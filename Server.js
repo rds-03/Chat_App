9ba6c7528b5befbd0fd759aa39b8234560ead671
Ruben Davis Saji <rubendavissaji2003@gmail.com>
2024-10-19 20:52:48 +0530

    Modified

--- Server.js
+++ Server.js
@@ -2,21 +2,62 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const http = require("http").createServer(app);
-// const cors = require("cors");
-// app.use(cors);
 const io = require("socket.io")(http);
+
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
-//socket
+const rooms = {};
+
 io.on("connection", (socket) => {
-  console.log("connected..");
-  socket.on("message", (msg) => {
-    socket.broadcast.emit("messagetoAll", msg);
+  console.log("A user connected..");
+
+  socket.on("createRoom", (roomId, username) => {
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+      rooms[roomId].push(username);
+      socket.join(roomId);
+      socket.roomId = roomId;
+      socket.username = username;
+
+      io.to(roomId).emit("roomUsers", rooms[roomId]);
+      console.log(`${username} created ${roomId}`);
+    }
   });
-  socket.on("disconnect", (socket) => {
-    console.log("user disconected");
+
+  socket.on("joinRoom", (roomId, username) => {
+    if (rooms[roomId]) {
+      rooms[roomId].push(username);
+      socket.join(roomId);
+      socket.roomId = roomId;
+      socket.username = username;
+
+      io.to(roomId).emit("roomUsers", rooms[roomId]);
+      console.log(`${username} joined room ${roomId}`);
+    } else {
+      socket.emit("errorMessage", "Room not found.");
+    }
+  });
+
+  socket.on("message", (msg) => {
+    const roomId = socket.roomId;
+    if (roomId) {
+      socket.broadcast.to(roomId).emit("messageToRoom", {
+        // Corrected event name
+        user: socket.username,
+        message: msg.message, // Fixed to send message content
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const roomId = socket.roomId;
+    if (roomId) {
+      rooms[roomId] = rooms[roomId].filter((user) => user !== socket.username);
+      io.to(roomId).emit("roomUsers", rooms[roomId]);
+      console.log(`${socket.username} disconnected from ${roomId}`);
+    }
   });
 });
 
--- public/user.js
+++ public/user.js
@@ -1,60 +1,98 @@
 const socket = io();
 let person;
+let roomId;
 const text = document.querySelector("#inpMssg");
 const MessageArea = document.querySelector(".Message_section");
 const Entry = document.querySelector(".Entry");
+
+const textArea = document.querySelector("#inpMssg");
+
+textArea.addEventListener("input", function () {
+  this.style.height = "auto"; // Reset height
+  this.style.height = this.scrollHeight + "px"; // Adjust height based on content
+});
+
 do {
   person = prompt("Enter your name: ");
 } while (!person);
 
-text.addEventListener("keyup", (e) => {
-  if (e.key == "Enter") {
-    sendMessage(e.target.value);
-  }
-});
+do {
+  roomId = prompt(
+    "Do you want to create a new room or join an existing one? \nEnter a room number:"
+  );
+} while (!roomId);
+
+let isRoomNew = confirm("Do you want to create this room?");
+if (isRoomNew) {
+  socket.emit("createRoom", roomId, person); // Create new room
+} else {
+  socket.emit("joinRoom", roomId, person); // Join existing room
+}
 
 const sendMessage = (message) => {
   let msg = {
     user: person,
     message: message,
   };
-  //Add message
-  Addmessaage(msg, "outgoing");
+  // Add message
+  AddMessage(msg, "outgoing");
   text.value = "";
   bottomScroll();
 
-  //
+  // Emit message event for this room
   socket.emit("message", msg);
 };
 
-const Addmessaage = (msg, type) => {
+const AddMessage = (msg, type) => {
   let MainDiv = document.createElement("div");
   let className = type;
   MainDiv.classList.add(className, "message");
 
-  let Markkup = `
+  let Markup = `
     <h4>${msg.user}</h4>
     <p>${msg.message}</p>
     `;
-  MainDiv.innerHTML = Markkup;
+  MainDiv.innerHTML = Markup;
 
   MessageArea.appendChild(MainDiv);
 };
 
-//Recieve Message
-socket.on("messagetoAll", (msg) => {
-  // console.log(`${msg.user} joined the chat`);
-  Addmessaage(msg, "incomming");
+// Send message on pressing Enter
+text.addEventListener("keyup", (e) => {
+  if (e.key == "Enter") {
+    sendMessage(e.target.value);
+  }
+});
+
+// Receive messages from the room
+socket.on("messageToRoom", (msg) => {
+  AddMessage(msg, "incoming");
   bottomScroll();
 });
 
-const NewUser = (msg) => {
-  var text = `${msg.user} Joined`;
-  var newElement = document.createElement("h3");
+// Function to add a new user to the active members list
+const NewUser = (user) => {
+  var text = `${user} joined the room`;
+  var newElement = document.createElement("div");
+  newElement.classList.add("join-message");
   var listText = document.createTextNode(text);
   newElement.appendChild(listText);
   Entry.appendChild(newElement);
 };
+// Update the list of users in the room
+socket.on("roomUsers", (users) => {
+  Entry.innerHTML = ""; // Clear the list before updating
+  users.forEach((user) => {
+    NewUser(user);
+  });
+});
+
+// Scroll to the bottom of the message area
 const bottomScroll = () => {
   MessageArea.scrollTop = MessageArea.scrollHeight;
 };
+
+// Handle error messages like room not found
+socket.on("errorMessage", (error) => {
+  alert(error);
+});
